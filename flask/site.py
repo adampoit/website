55d564f0a0f9d457196da9f8caa5c5d0688806dd
Adam Poit <metalkeys404@gmail.com>
2014-12-24 02:30:57 -0700

    Load model using Flask.

--- flask/site.py
+++ flask/site.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask.ext.cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/cube.json")
+def cube():
+    vertices = [
+        -1.0, -1.0, -1.0, #0
+        -1.0, -1.0,  1.0, #1
+        -1.0,  1.0, -1.0, #2
+        -1.0,  1.0,  1.0, #3
+         1.0, -1.0, -1.0, #4
+         1.0, -1.0,  1.0, #5
+         1.0,  1.0, -1.0, #6
+         1.0,  1.0,  1.0  #7
+    ]
+    indicies = [
+        1,  3,  5,    1,  3,  7, # Front face
+        0,  2,  4,    2,  4,  6, # Back face
+        2,  3,  7,    2,  6,  7, # Top face
+        0,  1,  5,    0,  4,  5, # Bottom face
+        4,  5,  7,    4,  6,  7, # Right face
+        0,  1,  3,    0,  2,  3  # Left face
+    ]
+    return jsonify(vertices = vertices, indicies = indicies)
 
 if __name__ == "__main__":
     app.run()

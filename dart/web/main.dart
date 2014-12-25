c06be55532d00e45e2b62e7862195173bc75c2b8
Adam Poit <metalkeys404@gmail.com>
2014-12-25 13:28:29 -0800

    Move model creation out of renderer.

--- dart/web/main.dart
+++ dart/web/main.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 import 'package:renderer/renderer.dart';
+import 'package:renderer/model.dart';
+import 'package:vector_math/vector_math.dart';
 
 void main()
 {
@@ -17,19 +19,22 @@ void main()
     canvas.height = window.innerHeight;
   });
   
+  var model = new Model();
+  model.init(gl, 'http://localhost:5000/cube.json');
+  
   var renderer = new Renderer(gl, window);
   
   window.onKeyDown.listen((e) 
   {
     if (e.keyCode == KeyCode.RIGHT)
-      renderer.MoveRight();
+      model.Position += new Vector3(0.0, 0.0, -0.1);
     if (e.keyCode == KeyCode.LEFT)
-      renderer.MoveLeft();
+      model.Position += new Vector3(0.0, 0.0, 0.1);
     if (e.keyCode == KeyCode.UP)
-      renderer.MoveUp();
+      model.Position += new Vector3(-0.1, 0.0, 0.0);
     if (e.keyCode == KeyCode.DOWN)
-      renderer.MoveDown();
+      model.Position += new Vector3(0.1, 0.0, 0.0);
   });
   
-  renderer.start();
+  renderer.start(model);
 }
